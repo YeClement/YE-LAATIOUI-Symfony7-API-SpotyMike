c257ea9726b65771b088a5a0dcd8523657b8dbc8
msylvest <mikechristopher.sylvestre.ext@eulerhermes.com>
2024-03-27 10:01:20 +0100

    🐛 FIX: Progree Entity

---
+++ src/Entity/Playlist.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlaylistRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PlaylistRepository::class)]
+class Playlist
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    // #[ORM\Id]
+    #[ORM\Column(length: 90)]
+    private ?string $idPlaylist = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $title = null;
+
+    #[ORM\Column]
+    private ?bool $public = true;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createAt = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $updateAt = null;
+
+    #[ORM\ManyToMany(targetEntity: Song::class, mappedBy: 'playlist_idPlaylist')]
+    private Collection $songs;
+
+    public function __construct()
+    {
+        $this->songs = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdPlaylist(): ?string
+    {
+        return $this->idPlaylist;
+    }
+
+    public function setIdPlaylist(string $idPlaylist): static
+    {
+        $this->idPlaylist = $idPlaylist;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function isPublic(): ?bool
+    {
+        return $this->public;
+    }
+
+    public function setPublic(bool $public): static
+    {
+        $this->public = $public;
+
+        return $this;
+    }
+
+    public function getCreateAt(): ?\DateTimeImmutable
+    {
+        return $this->createAt;
+    }
+
+    public function setCreateAt(\DateTimeImmutable $createAt): static
+    {
+        $this->createAt = $createAt;
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTimeInterface
+    {
+        return $this->updateAt;
+    }
+
+    public function setUpdateAt(\DateTimeInterface $updateAt): static
+    {
+        $this->updateAt = $updateAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Song>
+     */
+    public function getSongs(): Collection
+    {
+        return $this->songs;
+    }
+
+    public function addSong(Song $song): static
+    {
+        if (!$this->songs->contains($song)) {
+            $this->songs->add($song);
+            $song->addPlaylistIdPlaylist($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSong(Song $song): static
+    {
+        if ($this->songs->removeElement($song)) {
+            $song->removePlaylistIdPlaylist($this);
+        }
+
+        return $this;
+    }
+}
